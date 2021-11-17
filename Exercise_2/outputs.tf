ca456ade04bf03b41007dbd95d5dcbea93a0cae9
Guillermo Ampie <ga.ampie@gmail.com>
2021-11-17 16:18:07 -0600

    Add input variables

--- Exercise_2/outputs.tf
+++ Exercise_2/outputs.tf
@@ -1,1 +1,25 @@
 # TODO: Define the output variable for the lambda function.
+output "aws_region" {
+  description = "AWS Region selected"
+
+  value = var.aws_region
+}
+
+output "lambda_bucket_name" {
+  description = "S3 bucket name used to store the lambda function code"
+
+  value = aws_s3_bucket.lambda_bucket.id
+}
+
+output "lambda_function_name" {
+  description = "The lambda function name"
+
+  value = aws_lambda_function.greet.function_name
+}
+
+output "lambda_greet_arn" {
+  description = "The lambda function arn name"
+
+  value = aws_lambda_function.greet.arn
+}
+
--- Exercise_2/variables.tf
+++ Exercise_2/variables.tf
@@ -1,1 +1,6 @@
 # TODO: Define the variable for aws_region
+variable "aws_region" {
+  description = "The VPC region for regional-bound resources"
+  type        = string
+  default     = "us-east-1" # Default region
+}
